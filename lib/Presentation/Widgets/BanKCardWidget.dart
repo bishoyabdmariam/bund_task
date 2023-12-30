85ea9e7afd9dfa0ecbc0cb4c8088bcb4db77360a
Bishoy <bishoyabdo236@gmail.com>
2023-12-30 18:56:12 +0200

    I was forget sizedBox

--- lib/Presentation/Widgets/BanKCardWidget.dart
+++ lib/Presentation/Widgets/BanKCardWidget.dart
@@ -178,6 +178,7 @@ class _BankCardWidgetState extends State<BankCardWidget> {
                           ),
                       ],
                     ),
+                    SizedBoxes.box30height
                   ],
                 ),
               );
